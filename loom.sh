4de5dac9e60cb6243bb2b7a62ce9c0bb1fdc8098
Kirk Daniels <kdaniels@unca.edu>
2012-10-06 18:47:51 -0400

    prevents redundant email notifcations

--- loom.sh
+++ loom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEST_STRING=""
+
 source config
 
 if [ "`cat $TRIP_FILE`" = "true" ] ; then
@@ -8,23 +10,27 @@ else
   TRIP="false"
 
 #HTTP_CHECK
-  if [ $HTTP_CHECK = "yes" ] ; then
-    if [ `curl $CURL_OPTS $HTTP_CHECK_url` != ""  ] ; then
+  if [ "$HTTP_CHECK" = "yes" ] ; then
+    if [ "`curl $CURL_OPTS $HTTP_CHECK_url | grep $ID`" != ""  ] ; then
       TRIP="true"
+      TEST_STRING="HTTP_CHECK|"
     fi
   fi
 #HTTP_CHECK
 
 #MAIL_CHECK
-  if [ $CHECK_MAIL = "yes" ] ; then
+  if [ "$MAIL_CHECK" = "yes" ] ; then
     fetchmail -f fetchmailrc
-    if [ "`mail --print | grep $CHECK_MAIL_alertcode`" != "" ] ; then
+    if [ "`mail --print | grep $ID`" != "" ] ; then
       TRIP="true"
+      TEST_STRING=$TEST_STRING"MAIL_CHECK|"
     fi
   fi
 #MAIL_CHECK
 
-  if [ $TRIP = "true" ] ; then
+  echo $TEST_STRING
+
+  if [ "$TRIP" = "true" ] ; then
     echo true > $TRIP_FILE
     ./payload.sh
   fi
--- payload.sh
+++ payload.sh
@@ -2,28 +2,20 @@
 
 source config
 
-LAST_IP=`cat var/ip`
-
-if [ "$1" ] ; then
-  CURRENT_IP=$1
-else
-  CURRENT_IP=`curl $CURL_OPTS $IP_CHECK_url`
-fi
+LAST_IP=`cat $IP_FILE`" "
+CURRENT_IP=`curl $CURL_OPTS $IP_CHECK_url`
 
 #SEND_MAIL
-if [ $SEND_MAIL = "yes" ] ; then
-
-  echo "ID: $ID
-IP: $CURRENT_IP" | mail -s "airloom notification" $SEND_MAIL_addr -aFrom:$SEND_MAIL_fromaddr
-
+if [ "$SEND_MAIL" = "yes" ] && [ "$CURRENT_IP" != "$LAST_IP" ]; then
+  echo $CURRENT_IP > $IP_FILE
+  echo "ID: $ID | IP: $CURRENT_IP" | mail -s "airloom notification" $SEND_MAIL_addr -aFrom:$SEND_MAIL_fromaddr
 fi
 #SEND_MAIL
 
 #SSH
-if [ $SSH = "yes" ] ; then
-  if [ "`ps aux | grep $SSH_user@$SSH_host | grep ssh`" = "" ] ; then
-    ssh -N -f -R $SSH_rev -p $SSH_port $SSH_user@$SSH_host
-  fi
+if [ "$SSH" = "yes" ] && [ "`ps aux | grep $SSH_user@$SSH_host | grep ssh`" = "" ]; then
+  echo "SSH CLIENT STARTED"
+  ssh -N -f -R $SSH_rev -p $SSH_port $SSH_user@$SSH_host
 fi
 #SSH
 
